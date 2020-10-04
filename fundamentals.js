68103ced4150098846d75509c8374892485c741d
Huayu-Qin <445670669@qq.com>
2020-10-04 19:47:48 +0100

    Nested objects

--- fundamentals.js
+++ fundamentals.js
@@ -1,10 +1,14 @@
 const aCar = {
-    owner : 'Joe Bloggs',
-    type : {
-        make : 'Toyota',
-        model : 'Corolla',
-        cc : 1.8
+    owner: "Joe Bloggs",
+    type: {
+      make: "Toyota",
+      model: "Corolla",
+      cc: 1.8
     },
-    registration : '201-WD-1058'
-};
-console.log(aCar.owner + ' drives a ' + aCar.type.make);
+    registration: { year: 201, county: "WD", number: 1058 }
+  };
+  
+  console.log(aCar.owner + " drives a " + aCar['type']['make']);
+  console.log(
+    "Reg. = " + aCar.registration.year + "-" + aCar.registration.county + "-" + aCar.registration.number
+  );
