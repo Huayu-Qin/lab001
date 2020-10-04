0246ff317ef05098caf7838dd62738feeaef8b2f
Huayu-Qin <445670669@qq.com>
2020-10-04 19:54:41 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -12,3 +12,17 @@ const aCar = {
   console.log(
     "Reg. = " + aCar.registration.year + "-" + aCar.registration.county + "-" + aCar.registration.number
   );
+
+aCar.milage = 10000
+aCar.color = {
+  exterior: "red",
+  interior: { 
+      texture: "leather", 
+      shade: "cream" 
+    }
+}
+
+console.log(
+  "It is a " + aCar.color.exterior + " car, " + aCar.milage + " milage, with " + aCar.color.interior.texture + " interior."
+);
+  
